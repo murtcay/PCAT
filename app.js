15d9094d2a9c4d5bc67dc3935c3f565c091c4090
Murat ÇAY <murtcay@gmail.com>
2021-10-12 21:08:25 +0300

    get data from form request

--- app.js
+++ app.js
@@ -1,14 +1,35 @@
 const express = require('express');
+const ejs = require('ejs');
 const path = require('path');
 
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'));
+// TEMPALATE ENGINE
+app.set('view engine', 'ejs');
 
+// MIDDLEWARES
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+// ROUTES
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+  res.render('index');
 });
+
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
+app.get('/add-photo', (req, res) => {
+  res.render('add-photo');
+});
+
+app.post('/photos', (req, res) => {
+  console.log(req.body);
+  res.redirect('/');
+})
 
 app.listen(port, () => {
   console.log(`Server is listening on ${port}... `);
